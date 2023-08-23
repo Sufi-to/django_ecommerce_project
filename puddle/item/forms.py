a1c6aeb387f9fba8f379c0232d4e6e6c67c1d841
Tomas Tecle <3thomas225@gmail.com>
2023-08-23 20:47:01 +0400

    final version

---
+++ puddle/item/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+
+from .models import Item
+
+InputClasses = 'w-full py-4 px-6 rounded-xl border'
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('category', 'name', 'description', 'price', 'image')
+        widgets = {
+			'category': forms.Select(attrs={
+				'class': InputClasses
+			}),
+			'name': forms.TextInput(attrs={
+				'class': InputClasses
+			}),
+			'description': forms.Textarea(attrs={
+				'class': InputClasses
+			}),
+			'price': forms.TextInput(attrs={
+				'class': InputClasses
+			}),
+			'image': forms.FileInput(attrs={
+				'class': InputClasses
+			}),
+		}
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name', 'description', 'price', 'image', 'is_sold')
+        widgets = {
+			'name': forms.TextInput(attrs={
+				'class': InputClasses
+			}),
+			'description': forms.Textarea(attrs={
+				'class': InputClasses
+			}),
+			'price': forms.TextInput(attrs={
+				'class': InputClasses
+			}),
+			'image': forms.FileInput(attrs={
+				'class': InputClasses
+			}),
+		}
